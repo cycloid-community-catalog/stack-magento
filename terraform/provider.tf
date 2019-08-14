f824acc7cfd0b1cfff3942530c348e25a2182c0c
talset <talset55@gmail.com>
2019-08-14 15:46:30 +0200

    terraform: global file v12

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,26 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.aws_region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.aws_region
 }
 
-variable "customer" {}
-variable "project" {}
-variable "env" {}
+variable "customer" {
+}
 
-variable "access_key" {}
-variable "secret_key" {}
+variable "project" {
+}
+
+variable "env" {
+}
+
+variable "access_key" {
+}
+
+variable "secret_key" {
+}
 
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "eu-west-1"
 }
+
